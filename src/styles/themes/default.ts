7640a5191dd704efed55ea55d78ba85e3ee8b895
Leonam Silva <leonam.murilo@gmail.com>
2024-04-02 14:45:16 -0300

    feat: create global settings

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  'background': '#343434',
+
+  'white': '#ffffff',
+
+  'gray800': '#2b2b2b',
+  'gray600': '#3b3b3b',
+  'gray500': '#313131',
+  'gray400': '#494949',
+  'gray300': '#5f5f5f',
+  'gray200': '#7a7a7a',
+  'gray100': '#9f9f9f',
+
+  'green100': '#71bb00',
+
+  'red100': '#d65923',
+} as const
